df37f24bbd0d6226eae8b19ebdc2d40534a0618a
Alfred Cadawas <alfred.cadawas@gmail.com>
2026-05-19 13:50:54 -0400

    Updated to say hello to Nicole and MArcus

--- kristen.java
+++ kristen.java
@@ -1,6 +1,8 @@
 public class kristen{
 	public static void main(String[] args){
 		System.out.println("Hello Kristen!!!");
+		System.out.println("Hello Nicole!!!");
+		System.out.println("Hello Marcus!!!");
 	
 	}
 }
